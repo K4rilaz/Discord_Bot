bc5283902d4af1bac5c253b40270847607e5c58d
Karilaz <Karilaz@protonmail.com>
2022-03-10 14:43:54 +0100

    .

--- cogs/VotingSystem.py
+++ cogs/VotingSystem.py
@@ -1,18 +1,19 @@
 import discord
 from datetime import datetime
+
+from discord import Embed
 from discord.ext import commands, tasks
 
 
 
 
-owner_id = 664233463477698571
+
 class VotingSystem(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-        self.emoji = ['1/u20e3', '2/u20e3', '3/u20e3', '4/u20e3', '5/u20e3'
-                        '6/u20e3', '7/u20e3', '8/u20e3', '9/u20e3', '/U0001F51F']
-
+        self.emoji = ['1\u20e3', '2\u20e3', '3\u20e3', '4\u20e3', '5\u20e3',
+                      '6\u20e3', '7\u20e3', '8\u20e3', '9\u20e3', '\U0001F51F']
 
 
     @commands.Cog.listener()
@@ -21,27 +22,32 @@ class VotingSystem(commands.Cog):
 
 
     @commands.command()
-    async def poll (self,ctx, *, message):
+    @commands.has_permissions(administrator=True)
+    async def poll (self, ctx, time: int, vote: int, title, *options):
+        if len(options) > 10:
+            await ctx.send(':no_entry: You can only have **10 options** at maximum!')
 
+        elif time <= 15:
+            await ctx.send(':no_entry: Please provide poll end time greater than **15 minutes**!')
 
+        elif 51 < vote:
+            await ctx.send(':no_entry: Please provide poll vote max number less than ** 51**!')
 
+        polls = ['\n'.join([f'{self.emoji[index]} {options} \n' for index, option in enumerate(options)]), False]
 
+        embed = discord.Embed(title=title,
+                              description=f':stopwatch: Poll will end in **{time} minute**!',
+                              colour=0xF0000)
 
-        embed = discord.Embed(title="poll",
-                              description=f'{message}',
-                              colour=0xF000)
-        embed.set_thumbnail(url=f'https://cdn.discordapp.com/icons/{ctx.message.id}/{ctx.message.guild.icon}.png')
+        embed.set_thumbnail(url=f'https://cdn.discordapp.com/icons/{ctx.message.guild.id}/{ctx.message.guild.icon}.png')
+
+        name, value = polls
+        embed.add_field(name=name, value=value)
 
         message = await ctx.channel.send(embed=embed)
-        await message.add_reaction('yaay')
-        await message.add_reaction('nooo')
 
-
-
-
-
-
-
+        for item in self.emoji[:len(options)]:
+            await message.add_reaction(item)
 
 
 
